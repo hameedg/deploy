530d655e30a5606576e39b584b22db0e4693b0cb
hameedg <adilsaberi01@gmail.com>
2022-12-14 01:51:48 +0530

    login jobs and animation changes pushed

--- pages/jobs.jsx
+++ pages/jobs.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Cards from '../components/students/Cards';
 import Navbar from '../components/students/Navbar';
-import Raw from '../components/students/Raw';
 
 const jobs = () => {
   return (
     <>
       <Navbar />
       <Cards />
-      <Raw />
     </>
   );
 };
